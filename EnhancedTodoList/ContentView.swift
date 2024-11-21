3c6c602d62b7d6ead33abb0d3ed0346e74f81b8d
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2024-11-21 08:58:20 -0500

    Initial Commit

---
+++ EnhancedTodoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EnhancedTodoList
+//
+//  Created by Evelyn Chen on 2024-11-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
